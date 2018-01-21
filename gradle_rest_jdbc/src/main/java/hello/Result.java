b8f7830de5b11570f0866e347397fbb2f157e72a
CrazeeIvan <ciaranmaher@gmail.com>
2018-01-21 17:59:39 +0000

    v0.132 - Fixed dependency and Result class bug

--- gradle_rest_jdbc/src/main/java/hello/Result.java
+++ gradle_rest_jdbc/src/main/java/hello/Result.java
@@ -5,7 +5,7 @@ public class Result {
     private final long id;
     private final String content;
 
-    public Greeting(long id, String content) {
+    public Result(long id, String content) {
         this.id = id;
         this.content = content;
     }
