76de805462fb255a853759e7c5f680d3c08f67a7
CrazeeIvan <ciaranmaher@gmail.com>
2018-01-21 18:18:12 +0000

    v0.141 - More refactoring for poc

--- gradle_rest_jdbc/src/main/java/hello/ExampleService.java
+++ gradle_rest_jdbc/src/main/java/hello/ExampleService.java
@@ -12,54 +12,54 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleService {
-	@RequestMapping(value = "/employee/list",
+	@RequestMapping(value = "/customers/list",
 	        consumes = "application/json",
 	        method = { RequestMethod.POST })
-	public @ResponseBody Object getEmployeeList(
+	public @ResponseBody Object getCustomerList(
 	        ServletRequest req, ServletResponse res,
 	        @RequestBody String requestJson) throws Exception {
-		System.out.println("getEmployeeList() called");
+		System.out.println("getCustomerList() called");
 		return "{'response' : 'success'}";
 	}
 
-	@RequestMapping(value = "/employee/{employeeId}",
-	        consumes = "application/json",
-	        method = { RequestMethod.GET })
-	public @ResponseBody Object getEmployee(ServletRequest req,
-	        ServletResponse res,
-	        @PathVariable("employeeId") int employeeId)
-	        throws Exception {
-		System.out.println("getEmployee() called");
-		return "{'response' : 'success'}";
-	}
-
-	@RequestMapping(value = "/employee",
-	        consumes = "application/json",
-	        method = { RequestMethod.POST })
-	public @ResponseBody Object insertEmployee(
-	        ServletRequest req, ServletResponse res,
-	        @RequestBody String requestJson) throws Exception {
-		System.out.println("insertEmployee() called");
-		return "{'response' : 'success'}";
-	}
-
-	@RequestMapping(value = "/employee",
-	        consumes = "application/json",
-	        method = { RequestMethod.PUT })
-	public @ResponseBody Object updateEmployee(
-	        ServletRequest req, ServletResponse res,
-	        @RequestBody String requestJson) throws Exception {
-		System.out.println("updateEmployee() called");
-		return "{'response' : 'success'}";
-	}
-
-	@RequestMapping(value = "/employee",
-	        consumes = "application/json",
-	        method = { RequestMethod.DELETE })
-	public @ResponseBody Object deleteEmployee(
-	        ServletRequest req, ServletResponse res,
-	        @RequestBody String requestJson) throws Exception {
-		System.out.println("deleteEmployee() called");
-		return "{'response' : 'success'}";
-	}
+	// @RequestMapping(value = "/employee/{employeeId}",
+	//         consumes = "application/json",
+	//         method = { RequestMethod.GET })
+	// public @ResponseBody Object getEmployee(ServletRequest req,
+	//         ServletResponse res,
+	//         @PathVariable("employeeId") int employeeId)
+	//         throws Exception {
+	// 	System.out.println("getEmployee() called");
+	// 	return "{'response' : 'success'}";
+	// }
+	//
+	// @RequestMapping(value = "/employee",
+	//         consumes = "application/json",
+	//         method = { RequestMethod.POST })
+	// public @ResponseBody Object insertEmployee(
+	//         ServletRequest req, ServletResponse res,
+	//         @RequestBody String requestJson) throws Exception {
+	// 	System.out.println("insertEmployee() called");
+	// 	return "{'response' : 'success'}";
+	// }
+	//
+	// @RequestMapping(value = "/employee",
+	//         consumes = "application/json",
+	//         method = { RequestMethod.PUT })
+	// public @ResponseBody Object updateEmployee(
+	//         ServletRequest req, ServletResponse res,
+	//         @RequestBody String requestJson) throws Exception {
+	// 	System.out.println("updateEmployee() called");
+	// 	return "{'response' : 'success'}";
+	// }
+	//
+	// @RequestMapping(value = "/employee",
+	//         consumes = "application/json",
+	//         method = { RequestMethod.DELETE })
+	// public @ResponseBody Object deleteEmployee(
+	//         ServletRequest req, ServletResponse res,
+	//         @RequestBody String requestJson) throws Exception {
+	// 	System.out.println("deleteEmployee() called");
+	// 	return "{'response' : 'success'}";
+	// }
 }
