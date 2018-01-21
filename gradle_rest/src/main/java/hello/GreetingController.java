bced2474a79a3a3f2afb8c9e4905dc8621962abf
CrazeeIvan <ciaranmaher@gmail.com>
2018-01-21 16:48:26 +0000

    v0.13 - Added additional endpoints for proof of concept

--- gradle_rest/src/main/java/hello/GreetingController.java
+++ gradle_rest/src/main/java/hello/GreetingController.java
@@ -9,11 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private final AtomicLong greetingCounter = new AtomicLong();
+    private static final String template = "Farewell, %s!";
+    private final AtomicLong farewellCounter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
+        return new Greeting(greetingCounter.incrementAndGet(),
+                            String.format(template, name));
+    }
+    @RequestMapping("/farewell")
+    public Farewell Farewell(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Farewell(farewellCounter.incrementAndGet(),
                             String.format(template, name));
     }
 }
