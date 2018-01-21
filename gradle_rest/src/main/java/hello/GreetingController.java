d715245564c906e06026f795598bf3b2666e4657
CrazeeIvan <ciaranmaher@gmail.com>
2018-01-21 16:50:09 +0000

    v0.131 - Missed a string in adding farewell functionality

--- gradle_rest/src/main/java/hello/GreetingController.java
+++ gradle_rest/src/main/java/hello/GreetingController.java
@@ -8,19 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String greetingTemplate = "Hello, %s!";
     private final AtomicLong greetingCounter = new AtomicLong();
-    private static final String template = "Farewell, %s!";
+    private static final String farewellTemplate = "Farewell, %s!";
     private final AtomicLong farewellCounter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(greetingCounter.incrementAndGet(),
-                            String.format(template, name));
+                            String.format(greetingTemplate, name));
     }
     @RequestMapping("/farewell")
     public Farewell Farewell(@RequestParam(value="name", defaultValue="World") String name) {
         return new Farewell(farewellCounter.incrementAndGet(),
-                            String.format(template, name));
+                            String.format(farewellTemplate, name));
     }
 }
