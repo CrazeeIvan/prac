c630ed74c7bf2e8d2e8d9f55d0de41a416ab6b63
CrazeeIvan <ciaranmaher@gmail.com>
2018-01-21 18:33:27 +0000

    v0.143 - Troubleshooting db connection issue

--- gradle_rest_jdbc/src/main/java/hello/Application.java
+++ gradle_rest_jdbc/src/main/java/hello/Application.java
@@ -33,11 +33,11 @@ public class Application implements CommandLineRunner {
         jdbcTemplate.execute("CREATE TABLE customers(" +
                 "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
 
+        log.info("Table initialised");
         // Split up the array of whole names into an array of first/last names
         List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
                 .map(name -> name.split(" "))
                 .collect(Collectors.toList());
-
         // Use a Java 8 stream to print out each tuple of the list
         splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
 
