8949726556dde9d81479191e298f4e85a638167c
CrazeeIvan <ciaranmaher@gmail.com>
2018-01-21 19:12:41 +0000

    v0.149 - More troubleshooting jdbjc connection issue

--- gradle_rest_jdbc/src/main/java/hello/Application.java
+++ gradle_rest_jdbc/src/main/java/hello/Application.java
@@ -20,34 +20,34 @@ public class Application implements CommandLineRunner {
     public static void main(String args[]) {
         SpringApplication.run(Application.class, args);
     }
-    //
-    // @Autowired
-    // JdbcTemplate jdbcTemplate;
+    
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Override
     public void run(String... strings) throws Exception {
 
         log.info("Creating tables");
-
-        JdbcAccess.jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
-        JdbcAccess.jdbcTemplate.execute("CREATE TABLE customers(" +
-                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
-
-        log.info("Table initialised");
-        // Split up the array of whole names into an array of first/last names
-        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
-                .map(name -> name.split(" "))
-                .collect(Collectors.toList());
-        // Use a Java 8 stream to print out each tuple of the list
-        splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
-
-        // Uses JdbcTemplate's batchUpdate operation to bulk load data
-        JdbcAccess.jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
-
-        log.info("Querying for customer records where first_name = 'Josh':");
-        JdbcAccess.jdbcTemplate.query(
-                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
-                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
-        ).forEach(customer -> log.info(customer.toString()));
+        JdbcAccess(jdbcTemplate);
+    //     JdbcAccess.jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+    //     JdbcAccess.jdbcTemplate.execute("CREATE TABLE customers(" +
+    //             "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+    //
+    //     log.info("Table initialised");
+    //     // Split up the array of whole names into an array of first/last names
+    //     List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+    //             .map(name -> name.split(" "))
+    //             .collect(Collectors.toList());
+    //     // Use a Java 8 stream to print out each tuple of the list
+    //     splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+    //
+    //     // Uses JdbcTemplate's batchUpdate operation to bulk load data
+    //     JdbcAccess.jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+    //
+    //     log.info("Querying for customer records where first_name = 'Josh':");
+    //     JdbcAccess.jdbcTemplate.query(
+    //             "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
+    //             (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+    //     ).forEach(customer -> log.info(customer.toString()));
     }
 }
