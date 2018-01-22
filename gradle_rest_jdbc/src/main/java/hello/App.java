66167d82715e35cc9e3b0a7f002afe30917427ab
CrazeeIvan <ciaranmaher@gmail.com>
2018-01-22 21:18:38 +0000

    v0.1499 - Trying to restart the reactor

--- gradle_rest_jdbc/src/main/java/hello/App.java
+++ gradle_rest_jdbc/src/main/java/hello/App.java
@@ -11,6 +11,20 @@ import com.hello.services.PersonService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
+
 @SpringBootApplication
 public class App {
 
