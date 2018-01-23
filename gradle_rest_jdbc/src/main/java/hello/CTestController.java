e3bf169cc8418172f1c17e10183d8a75028badfb
CrazeeIvan <ciaranmaher@gmail.com>
2018-01-23 21:01:58 +0000

    v0.151 - Added restful test command and removed unnecessary class

---
+++ gradle_rest_jdbc/src/main/java/hello/CTestController.java
@@ -0,0 +1,26 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CTestController {
+
+    private static final String greetingTemplate = "Hello, %s!";
+    private final AtomicLong greetingCounter = new AtomicLong();
+    private static final String farewellTemplate = "Farewell, %s!";
+    private final AtomicLong farewellCounter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(greetingCounter.incrementAndGet(),
+                            String.format(greetingTemplate, name));
+    }
+    @RequestMapping("/farewell")
+    public Farewell Farewell(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Farewell(farewellCounter.incrementAndGet(),
+                            String.format(farewellTemplate, name));
+    }
+}
