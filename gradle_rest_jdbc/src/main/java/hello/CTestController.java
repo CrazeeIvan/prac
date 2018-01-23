82cac8c959faf1caf15547a06284bda41c251008
CrazeeIvan <ciaranmaher@gmail.com>
2018-01-23 22:33:51 +0000

    v0.156 - Fixed person dependencies

--- gradle_rest_jdbc/src/main/java/hello/CTestController.java
+++ gradle_rest_jdbc/src/main/java/hello/CTestController.java
@@ -4,6 +4,8 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.hello.bean.Person;
+import com.hello.services.PersonService;
 
 @RestController
 public class CTestController {
